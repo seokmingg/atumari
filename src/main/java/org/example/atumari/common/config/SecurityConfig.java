2dbd0fea065be0cd3eae93dba6c7fc2617dc04be
seokmin <devhong0016@gmail.com>
2026-09-06 15:26:33 +0900

    fix: 지도불러오기 허용

--- src/main/java/org/example/atumari/common/config/SecurityConfig.java
+++ src/main/java/org/example/atumari/common/config/SecurityConfig.java
@@ -15,6 +15,9 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers
+                        .frameOptions(frameOptions -> frameOptions.sameOrigin())
+                )
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 
         return http.build();
