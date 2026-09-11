cc440044cb2fb94805ae391fe768ec948e769b54
KimGyeonkang <v.tang3203@gmail.com>
2026-09-11 09:41:44 +0900

    feat: LogoutController 추가 및 작성 중

---
+++ src/main/java/org/example/atumari/member/controller/LogoutController.java
@@ -0,0 +1,32 @@
+package org.example.atumari.member.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class LogoutController extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		String loginName = (String)session.getAttribute("sessionName");
+		
+		// 세션 삭제 후 로그아웃 처리
+		session.invalidate();
+		
+		request.getRequestDispatcher("/WEB-INF/views/home/index.jsp")
+			.forward(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
